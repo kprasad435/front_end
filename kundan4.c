d473ad558ac18cf83934528f49fcfe945b6348e1
kprasad435 <kundanprasad6118@gmail.com>
2025-08-19 14:56:27 +0530

    Add files via upload

---
+++ kundan4.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+int main (){
+            int m;
+            printf("enter your number");
+            scanf("%d",&m);
+            if(m>90){
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
---
+++ united.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main() {
+    // declaring the value of integer value
+    int a;
+    int b;
+    int c;
+    int d;
+
+    //taking input in the variable a
+    printf("enter first value");
+    scanf("%d",&a);
+
+    printf("enter second value");
+    scanf("%d",&b);
+    c=a+b;
+    d=a+b;
+    //printing the value of a
+    printf("sum =");
+    printf("%d",c);
+    printf("product");
+    printf("%d",d);
+}
+
+
